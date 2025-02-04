80c9a2fce72c583550bb65897de87fca16a55672
animeshkumar29 <animesh.kumar@triconinfotech.com>
2025-02-04 18:26:03 +0530

    Updating index.ts file

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 import FeroxbusterModule from './feroxBusterModule';
 
-export default FeroxbusterModule;
+export async function scanAndGenerateReports(apiKey: string, apiEndpoint: string, systemUrl: string, wordlistUrl: string) {
+    const module = new FeroxbusterModule(apiKey, apiEndpoint);
+    return module.scanAndGenerateReports(systemUrl, wordlistUrl);
+}
+
+export { FeroxbusterModule };  // Named export for the class
+export default FeroxbusterModule;  // Default export
